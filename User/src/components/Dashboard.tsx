28eb5f61344c7ed949383ceea02d130913d23276
Dinesh Gamage <dinesh@ecyber.com>
2021-01-08 17:55:38 +0530

    extra tiles added to the landing page

--- User/src/components/Dashboard.tsx
+++ User/src/components/Dashboard.tsx
@@ -22,7 +22,79 @@ const Dashboard: React.FunctionComponent<IDashboardProps> = (props) => {
 
             <div className="content">
 
+                <div className="title">Things to do</div>
+
+                <div className="quick-actions">
+                    <div className="quick-action-thumbnail-cont">
+                        <div className="quick-action-thumbnail watch">
+                            <div className="top"></div>
+                            <div className="bottom">
+                                <div className="label">
+                                    <div className="icon-cont">
+                                        <div className="icon"></div>
+                                    </div>
+
+                                    <div className="title">
+                                        <span className="small">Islamic traditions</span> <br />
+                                        <span className="large">Learn more</span>
+                                    </div>
+                                </div>
+                                <div className="actions">
+                                    <button className="btn scan" >PLAY</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="quick-action-thumbnail-cont">
+                        <div className="quick-action-thumbnail hijab">
+                            <div className="top"></div>
+                            <div className="bottom">
+                                <div className="label">
+                                    <div className="icon-cont">
+                                        <div className="icon"></div>
+                                    </div>
+
+                                    <div className="title">
+                                        <span className="small">Islamic traditions</span> <br />
+                                        <span className="large">Learn more</span>
+                                    </div>
+                                </div>
+                                <div className="actions">
+                                    <button className="btn scan" >PLAY</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="quick-actions large">
+
+                    <div className="quick-action-thumbnail-cont">
+                        <div className="quick-action-thumbnail map">
+                            <div className="top"></div>
+                            <div className="bottom">
+
+                                <div className="icon"></div>
+                                <div className="content">
+                                    <span> Next bus leaves in</span>
+                                    <div className="large">12 minutes</div>
+                                    <span>  to bada'ah</span>
+                                </div>
+
+
+                                <div className="actions">
+                                    <button className="btn scan" >reserve a seat</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
                 <div className="title">quick actions</div>
+
                 <div className="quick-actions">
                     <div className="quick-action-thumbnail-cont">
                         <div className="quick-action-thumbnail report">
@@ -45,7 +117,48 @@ const Dashboard: React.FunctionComponent<IDashboardProps> = (props) => {
                         </div>
                     </div>
 
+                    <div className="quick-action-thumbnail-cont">
+                        <div className="quick-action-thumbnail cab-book">
+                            <div className="header">
+                                <div className="icon-cont">
+                                    <div className="icon"></div>
+                                </div>
+                            </div>
+                            <div className="content">
+                                <span> Next cab to board</span>
+                                <div className="large">375 VLS</div>
+                                <span>  white toyota innova</span>
+                            </div>
+
+                            <div className="actions">
+                                <button className="btn notify-patrol">Book</button>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+
+                <div className="quick-actions large">
+
+                    <div className="quick-action-thumbnail-cont">
+                        <div className="quick-action-thumbnail prayer">
+                            <div className="top"></div>
+                            <div className="bottom">
+                                <div className="time">12: 00 PM</div>
+                                <div className="desc">
+                                    <div className="large">Zuhr</div>
+                                    <div className="text">attend nearest mosque</div>
+                                </div>
+                                <div className="actions">
+                                    <button className="btn scan" >View Timings</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+
             </div>
 
         </div>)
